5a6db4db2a9fb2b7160950f71ca1cffca36bc47d
Chris van der Merwe <chrisvdm0410@gmail.com>
2021-05-06 07:49:40 +0200

    Update typeorm.module.ts

--- src/config/typeorm.module.ts
+++ src/config/typeorm.module.ts
@@ -2,7 +2,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 export default TypeOrmModule.forRoot({
     type: 'mongodb',
-    // url: 'mongodb+srv://root:7wHYH4Uke4hqPRhn@cluster0.xpxnc.mongodb.net/personal-crm?retryWrites=true&w=majority',
     url: 'mongodb://localhost/personal-crm',
     synchronize: true,
     useUnifiedTopology: true,
