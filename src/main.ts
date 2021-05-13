30c5c1953c6ba3d27e779d265fc254bfdfecc3f0
Christiaan van der Merwe <chrisvdm0410@gmail.com>
2021-05-13 08:26:52 +0200

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+
+const serverConfig = config.get('server');
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+
+    if (process.env.NODE_ENV === 'development') {
+        app.enableCors();
+    } else {
+        app.enableCors({
+            origin: serverConfig.origin,
+        });
+    }
+
     await app.listen(3000);
 }
 bootstrap();
